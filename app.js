a7949ef190017d5304175ceae35d0c668c244cae
Matheus F <ma_ferreira21@hotmail.com>
2022-06-27 21:10:52 -0300

    ADD show current search value msg

--- app.js
+++ app.js
@@ -2,28 +2,30 @@ function main() {
     const input = document.querySelector('.valor');
     const btnSend =  document.querySelector('.send');
     const total = document.querySelector('.total');
+    const msg = document.querySelector('.result-msg')
+    // TODO add way to show the previous results
 
     btnSend.addEventListener('click', () => {
         if (!input.value) return;
-        getDigitalRoot(input.value)
-        input.value = ''
-        input.focus();
+        start()
     });
 
     input.addEventListener('keypress', (e)=> {
         if (!input.value) return;
         if (e.keyCode === 13) {
-            getDigitalRoot(input.value)
-            input.value = ''
-            input.focus();
-            //TODO Colocar a função
+        start()
         }
     });
 
+    function start() {
+        msg.innerText = `The digital root of "${input.value}" is:`
+        getDigitalRoot(input.value)
+        input.value = ''
+        input.focus();
+        }
+
     function getDigitalRoot(value) {
-        if (isNaN(value)) {
-            return
-        };
+        if (isNaN(value)) return;
         let final = handleValue(value)
         total.innerText = final
     }
@@ -32,9 +34,7 @@ function main() {
         let list = []
         for (i of value) list.push(parseInt(i));
         let total = list.reduce((e, i) => e + i, 0)
-        if (total >= 10) {
-            return handleValue(total.toString()) 
-        }
+        if (total >= 10) return handleValue(total.toString()) 
         return total
     }
 }
